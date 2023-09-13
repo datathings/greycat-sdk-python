35396e05ed6a45ab899ba5eeeee0ed1d0840de03
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-13 15:42:38 +0200

    Typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="greycat",
     version="0.0.0",
-    author="DataThings s.a",
+    author="DataThings S.A.",
     author_email="contact@datathings.com",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
