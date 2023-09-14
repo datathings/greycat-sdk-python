a2fdc0fede4bd67230a22def790769fe682e7366
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 14:49:11 +0200

    Investigating sed substitutions

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ rm -rf dist
 
 sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 sed -i -e "s/version = \"0.0.0\"/version = \"${VERSION}\"/" pyproject.toml
+grep version setup.py pyproject.toml
 
 python -m build -w
