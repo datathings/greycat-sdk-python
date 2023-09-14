7c5a647dc838b89af147979b3f677b59e74c0876
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 09:55:34 +0200

    Simplifying sed

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -ex
 
 VERSION=${VERSION:-"0.0.0"}
 
 rm -rf dist
 
-sed -i -e "s/^    version=\"0.0.0\",$/    version=\"${VERSION}\",/" setup.py
+sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 
 python -m build -w
