3501c4e4097d8a4db17bdb7db1a8176ee583753f
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 14:34:08 +0200

    Fix?

--- build.sh
+++ build.sh
@@ -8,6 +8,4 @@ rm -rf dist
 sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 sed -i -e "s/version = \"0.0.0\"/version = \"${VERSION}\"/" pyproject.toml
 
-python -m pip install --upgrade pip
-python -m pip install build
 python -m build -w
