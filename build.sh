6d26f90c562f236da54334315059ce4750d2f4ec
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 14:26:10 +0200

    Back to real config

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ rm -rf dist
 sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 sed -i -e "s/version = \"0.0.0\"/version = \"${VERSION}\"/" pyproject.toml
 
+python -m pip install --upgrade pip
+python -m pip install build
 python -m build -w
