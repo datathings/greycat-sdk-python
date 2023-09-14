67048b729ac98632be723d3f38bc4dabe975d613
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 09:25:32 +0200

    Get sh version

--- build.sh
+++ build.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-echo "DEBUG: 0"
-
 set -ex
-
-echo "DEBUG: 1"
 
 VERSION=${VERSION:-"0.0.0"}
 
-echo "DEBUG: 2"
-
 rm -rf dist
-
-echo "DEBUG: 3"
 
 sed -i -e "s/^    version=\"0.0.0\",$/    version=\"${VERSION}\",/" setup.py
 
-echo "DEBUG: 4"
-
 python -m build -w
-
-echo "DEBUG: 5"
