b7dc4810b9c70240021b38779e7b6d606f80d15b
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-14 09:23:47 +0200

    Have CI provide information

--- build.sh
+++ build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+
+echo "DEBUG: 0"
+
 set -ex
+
+echo "DEBUG: 1"
 
 VERSION=${VERSION:-"0.0.0"}
 
+echo "DEBUG: 2"
+
 rm -rf dist
+
+echo "DEBUG: 3"
 
 sed -i -e "s/^    version=\"0.0.0\",$/    version=\"${VERSION}\",/" setup.py
 
+echo "DEBUG: 4"
+
 python -m build -w
+
+echo "DEBUG: 5"
