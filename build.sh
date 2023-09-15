2ca33f9c857292571ef83a8010199faedfcc399d
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-15 16:40:54 +0200

    Fix

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ rm -rf build dist src/greycat.egg-info
 sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 
 python -m build -w
+
+mv "dist/GreyCat-${VERSION}-py3-none-any.whl" "dist/greycat-${VERSION}-py3-none-any.whl"
