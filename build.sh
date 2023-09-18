043a7136943760621e0aa59976f830519936a08a
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-18 08:48:22 +0200

    CI fixes

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -ex
 
-WHL_VERSION=$(echo ${VERSION:-"0.0.0"} | sed 's/-/+/')
-VERSION=${VERSION:-"0.0.0"}
+VERSION=$(echo ${VERSION:-"0.0.0"} | sed 's/-/+/')
 
 rm -rf build dist src/greycat.egg-info
 
 sed -i -e "s/version=\"0.0.0\",/version=\"${VERSION}\",/" setup.py
 
 python -m build -w
-
-mv "dist/GreyCat-${WHL_VERSION}-py3-none-any.whl" "dist/greycat-${WHL_VERSION}-py3-none-any.whl"
