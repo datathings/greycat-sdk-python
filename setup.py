634221717a86f71e473ae6f19ce0f825fdb41c28
Alexandre Garnier <alexandre.garnier@datathings.com>
2023-09-19 13:29:03 +0200

    Apache License 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.0",
     author="DataThings S.A.",
     author_email="contact@datathings.com",
-    license="https://creativecommons.org/licenses/by-nd/4.0/",
+    license="https://www.apache.org/licenses/LICENSE-2.0.html",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.8",
