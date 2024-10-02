3aea50f8e2ccdd93d8e32f659a07d9ab81260540
Alexandre Garnier <alexandre.garnier@datathings.com>
2024-10-02 11:12:11 +0200

    __object_read any field

--- src/greycat/greycat.py
+++ src/greycat/greycat.py
@@ -749,7 +749,7 @@ class GreyCat:
                     loaded_field = field_type.loader(field_type, stream)
                 else:
                     loaded_field = GreyCat._Stream._PRIMITIVE_LOADERS[
-                        att.sbi_type.value
+                        load_type
                     ](stream)
                 if att.mapped:
                     attributes[att.mapped_att_offset] = loaded_field
