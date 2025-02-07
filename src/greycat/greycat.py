c0f62226868d4fd3a72f1c8d47b2d459aed15e63
Alexandre Garnier <alexandre.garnier@datathings.com>
2025-02-07 16:49:17 +0100

    Cleanup

--- src/greycat/greycat.py
+++ src/greycat/greycat.py
@@ -18,33 +18,9 @@ try:
     import flask.typing
 
     class GreyCatServer(flask.Flask):
-        def __init__(
-            self,
-            import_name: str,
-            gc_abi_path: str,
-            static_url_path: str | None = None,
-            static_folder: str | os.PathLike[str] | None = "static",
-            static_host: str | None = None,
-            host_matching: bool = False,
-            subdomain_matching: bool = False,
-            template_folder: str | os.PathLike[str] | None = "templates",
-            instance_path: str | None = None,
-            instance_relative_config: bool = False,
-            root_path: str | None = None,
-        ):
-            super().__init__(
-                import_name=import_name,
-                static_url_path=static_url_path,
-                static_folder=static_folder,
-                static_host=static_host,
-                host_matching=host_matching,
-                subdomain_matching=subdomain_matching,
-                template_folder=template_folder,
-                instance_path=instance_path,
-                instance_relative_config=instance_relative_config,
-                root_path=root_path,
-            )
-            self.before_request_funcs  = {None: [self.unwrap_payload]}
+        def __init__(self, import_name: str, gc_abi_path: str, *kwargs):
+            super().__init__(import_name=import_name, *kwargs)
+            self.before_request_funcs = {None: [self.unwrap_payload]}
             self.bio: BytesIO = BytesIO()
             self.stream: GreyCat._Stream = GreyCat._Stream(
                 GreyCat(gc_abi_path), self.bio)
@@ -67,7 +43,6 @@ try:
                 self.bio.seek(0)
                 self.bio.truncate(0)
                 return unwrapped
-        
 
 
 except ModuleNotFoundError:
