30bd1507b2ee7d01763df3c62e932cafccc6e0dd
François Fouquet <francois.fouquet@datathings.com>
2025-07-24 18:52:25 +0200

    fix?

--- greycat/std_n.py
+++ greycat/std_n.py
@@ -1566,8 +1566,7 @@ class std_n:
                 if gc is None:
                     gc = GreyCat._DEFAULT
                 type_: GreyCat.Type = gc.types_by_name["core::Table"]
-                table: std_n.core._Table = type_.factory(type_, None)
-                table.data = nda
+                table: std_n.core._Table = type_.factory(type_, nda)
                 return table
 
             @staticmethod
